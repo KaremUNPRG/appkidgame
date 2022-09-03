6fc97df9c457b431e8b6426946b3804b993607c1
StevenAcademico <102645182+KaremUNPRG@users.noreply.github.com>
2022-09-02 22:01:48 -0500

    Revert "welcome"
    This reverts commit a09d9c3c0554d737b9a23024099ba995db6c7518.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
 
 <section class="ftco-section bg-light">
     <div class="container">
-        <div class="row justify-content-center  pb-2">
+        <div class="row justify-content-center mb-5 pb-2">
             <div class="col-md-8 text-center heading-section ftco-animate">
-                <h2 class="mb-4"><span>Juegos</span></h2>
+                <h2 class="mb-4"><span>Juegos</span> Recientes</h2>
             </div>
         </div>
         <div class="row renderHtmlListaJuego">
