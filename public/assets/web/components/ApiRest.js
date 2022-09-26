745e291d41ac5133ccbf56c6e525c309013afe9c
KaremUNPRG <102645182+KaremUNPRG@users.noreply.github.com>
2022-09-26 01:14:26 -0500

    ApiRuta

--- public/assets/web/components/ApiRest.js
+++ public/assets/web/components/ApiRest.js
@@ -6,8 +6,4 @@ export const headersList =  () => {
        };
 }
 
-<<<<<<< HEAD
-export const url =  "http://127.0.0.1/api";
-=======
 export const url =  "https://appkidgame-production.up.railway.app/api";
->>>>>>> b8bc3265efb1856d7851af2d43c73289c382e804
